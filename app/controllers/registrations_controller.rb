af1cc226223b356bd835841cc9d6354372c28a14
Viktor Justo Vasquez <viktor@vjustov.me>
2016-05-05 09:22:04 -0400

    Adds some email validations and complicate a bit the code.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,8 +8,13 @@ class RegistrationsController < ApplicationController
 
   def create
     if Event.first.seat_limit > Registration.count
-      if Registration.where(email:params[:registration][:email]).count < 1
-        new_registration = Registration.create(params.require(:registration).permit(:email, :first_name, :last_name, :phone_number).merge({ event_id: params[:event_id] }))
+      registration_parameters = params.require(:registration).permit(:email, :first_name, :last_name, :phone_number).merge({ event_id: params[:event_id] })
+      @registration = Registration.new(registration_parameters)
+
+      unless @registration.save
+        flash.now[:error] = "Email already in use."
+        @event = Event.find(params[:event_id])
+        render 'events/show'
       end
     end
   end
--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,5 +1,24 @@
 class Registration < ActiveRecord::Base
+
+  class EmailValidator < ActiveModel::Validator
+    def validate(registration)
+      if invalid_email?(registration)
+        product.errors[:box] << "cannot be empty if product is cropped"
+      end
+    end
+
+    private
+
+    def invalid_email?
+      !(registration.email =~ /[\w.-]+@[a-z]+\.[a-z]+/)
+    end
+  end
+
   belongs_to :event
+
+  # validates_with EmailValidator
+  # validates_uniqueness_of :email
+  validates :email, uniqueness: true, format: { with: /[\w.-]+@[a-z]+\.[a-z]+/ }
+
   # enum events: [:ruby_do, :javascript_do]
-  #validates_uniqueness_of :email
 end
