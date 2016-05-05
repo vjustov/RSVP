d549930772e6bd39634210cc27bfba43f6b86d6a
Viktor Justo Vasquez <viktor@vjustov.me>
2016-05-05 00:03:30 -0400

    Adds Registrations#index page.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,12 @@
 class RegistrationsController < ApplicationController
 
+  before_action :authenticate_user!, only: :index
+
+  def index
+    @registrations = Registration.where(email: current_user.email)
+  end
+
   def create
-    require 'pry'; binding.pry
     if Event.first.seat_limit > Registration.count
       if Registration.where(email:params[:registration][:email]).count < 1
         new_registration = Registration.create(params.require(:registration).permit(:email, :first_name, :last_name, :phone_number).merge({ event_id: params[:event_id] }))
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'events#index'
 
   resources :events, only: [:index, :show] do
     resources :registrations, only: [:create]
   end
+
+  resources :registrations, only: [:index]
 
   resources :event_registrations, only: [:index]
 
